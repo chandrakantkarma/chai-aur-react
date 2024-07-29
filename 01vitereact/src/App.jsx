ad7d710f85a66561c56effdffb71dbd8ee407840
Chandrakant Karma <shivamkarma80@gmail.com>
2024-07-29 19:48:53 +0530

    jsx and createElement

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,12 @@
+import React from 'react'
 
 
 function App() {
 
+  const username = "Chai or code"
   return (
     <h1>
-      chai aur React | CK
+      chai aur React | CK {username}
     </h1>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+  <div>
+    <h1>Custom App | Chai</h1>
+  </div>
+  )
+}
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https: //google.com',
+//       target: '_blank'
+//   },
+//   Children: 'Click me to visit google'
+// } 
+// const anotherElement = (
+//     <a href="https: //google.com" target='_blank'>Visit Google</a>
+// )
+
+const anotherElement = "Chai aur Code"
+const reactElement = React.createElement(
+    'a',
+    {href: 'https: //google.com', target: '_blank'},
+    'click me to visit google',
+    anotherElement
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    // reactElement;
+    // <MyApp/>
+    <App/>
+  
 )
